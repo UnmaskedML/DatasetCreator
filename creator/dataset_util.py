fb9a6d4f28f19796e757d4e31683e75843473f05
GrantPerkins <gcperk20@gmail.com>
2021-10-26 12:08:20 -0400

    Finish conversion script

--- creator/dataset_util.py
+++ creator/dataset_util.py
@@ -1,26 +1,25 @@
 import tensorflow as tf
 
+
 def int64_feature(value):
-  return tf.compat.v1.train.Feature(int64_list=tf.compat.v1.train.Int64List(value=[value]))
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
 
 def int64_list_feature(value):
-  return tf.compat.v1.train.Feature(int64_list=tf.compat.v1.train.Int64List(value=value))
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
 
 
 def bytes_feature(value):
-  return tf.compat.v1.train.Feature(bytes_list=tf.compat.v1.train.BytesList(value=[value]))
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 
 def bytes_list_feature(value):
-  return tf.compat.v1.train.Feature(bytes_list=tf.compat.v1.train.BytesList(value=value))
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=value))
 
 
 def float_feature(value):
-  return tf.compat.v1.train.Feature(float_list=tf.compat.v1.train.FloatList(value=[value]))
+    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
 
 
 def float_list_feature(value):
-  return tf.compat.v1.train.Feature(float_list=tf.compat.v1.train.FloatList(value=value))
-
-
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
--- creator/tfrecord.py
+++ creator/tfrecord.py
@@ -6,7 +6,7 @@ import io
 import dataset_util
 
 
-def create_tf_example(filename):
+def create_tf_example(filename, rows):
     with tf.io.gfile.GFile(filename, 'rb') as fid:
         encoded_jpg = fid.read()
     encoded_jpg_io = io.BytesIO(encoded_jpg)
@@ -15,21 +15,21 @@ def create_tf_example(filename):
     filename = filename.encode('utf8')
     image_format = b'jpeg'
 
-    xmins = []  # List of normalized left x coordinates in bounding box (1 per box)
-    xmaxs = []  # List of normalized right x coordinates in bounding box
+    xmins = [i["xmin"] for i in rows]  # List of normalized left x coordinates in bounding box (1 per box)
+    xmaxs = [i["xmax"] for i in rows]  # List of normalized right x coordinates in bounding box
     # (1 per box)
-    ymins = []  # List of normalized top y coordinates in bounding box (1 per box)
-    ymaxs = []  # List of normalized bottom y coordinates in bounding box
+    ymins = [i["ymin"] for i in rows]  # List of normalized top y coordinates in bounding box (1 per box)
+    ymaxs = [i["ymax"] for i in rows]  # List of normalized bottom y coordinates in bounding box
     # (1 per box)
-    classes_text = []  # List of string class name of bounding box (1 per box)
-    classes = []  # List of integer class id of bounding box (1 per box)
+    classes_text = [i["label"].encode("utf8") for i in rows]  # List of string class name of bounding box (1 per box)
+    classes = [1 for _ in rows]  # List of integer class id of bounding box (1 per box)
 
     tf_example = tf.train.Example(features=tf.train.Features(feature={
         'image/height': dataset_util.int64_feature(height),
         'image/width': dataset_util.int64_feature(width),
-        'image/filename': dataset_util.bytes_feature(filename),
-        'image/source_id': dataset_util.bytes_feature(filename),
-        'image/encoded': dataset_util.bytes_feature(encoded_jpg_io),
+        'image/filename': dataset_util.bytes_feature(bytes(filename)),
+        'image/source_id': dataset_util.bytes_feature(bytes(filename)),
+        'image/encoded': dataset_util.bytes_feature(encoded_jpg),
         'image/format': dataset_util.bytes_feature(image_format),
         'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
         'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
@@ -42,20 +42,25 @@ def create_tf_example(filename):
 
 
 def main():
+    images = {}
+    writer = tf.compat.v1.python_io.TFRecordWriter("data/tf/masks.tfrecord")
+    df = pd.read_csv("data/labels/masked_faces.csv")
+    for index, row in df.iterrows():
+        key = row["key"]
+        try:
+            images[key].append(row)
+        except KeyError:
+            images.update({key: [row]})
     examples = []
-    writer = tf.compat.v1.python_io.TFRecordWriter("data")
-    with pd.read_csv("data/labels/masked_faces.csv") as df:
-        for index, row in df.iterrows():
-            key = row["key"]
-            example = create_tf_example("data/images/masked_faces/" + key)
-            examples.append(example)
+    for image in images.keys():
+        example = create_tf_example("data/images/masked_faces/" + key, images[image])
+        examples.append(example)
 
     for example in examples:
-        tf_example = create_tf_example(example)
-        writer.write(tf_example.SerializeToString())
+        writer.write(example.SerializeToString())
 
     writer.close()
 
 
 if __name__ == '__main__':
-    tf.app.run()
+    main()
