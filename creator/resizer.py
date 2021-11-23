ebe1d565fea4ce45c6bbb8d2768c7c3bf13d8a62
JCharante <git@jcharante.com>
2021-11-23 17:34:28 -0500

    update resizer to support new dataset

--- creator/resizer.py
+++ creator/resizer.py
@@ -3,6 +3,30 @@ import cv2
 import os
 import numpy as np
 
+class MaskCSVRow:
+    def __init__(self, img, img_height, img_width, face_xtl, face_ytl, face_xbr, face_ybr,
+                 mask_xtl, mask_ytl, mask_xtm, mask_ytm, mask_xtr, mask_ytr, mask_xbr,
+                 mask_ybr,mask_xbm,mask_ybm,mask_xbl,mask_ybl):
+        self.img = img
+        self.img_height = img_height
+        self.img_width = img_width
+        self.face_xtl = face_xtl
+        self.face_ytl = face_ytl
+        self.face_xbr = face_xbr
+        self.face_ybr = face_ybr
+        self.mask_xtl = mask_xtl
+        self.mask_ytl = mask_ytl
+        self.mask_xtm = mask_xtm
+        self.mask_ytm = mask_ytm
+        self.mask_xtr = mask_xtr
+        self.mask_ytr = mask_ytr
+        self.mask_xbr = mask_xbr
+        self.mask_ybr = mask_ybr
+        self.mask_xbm = mask_xbm
+        self.mask_ybm = mask_ybm
+        self.mask_xbl = mask_xbl
+        self.mask_ybl = mask_ybl
+
 class CSVFile:
     def __init__(self, filename):
         self.filename = filename
@@ -27,7 +51,27 @@ class CSVFile:
         return ret
 
     def get_all_where_key_equals(self, key):
-        return [(line[0], line[1], int(line[2]), int(line[3]), int(line[4]), int(line[5])) for line in self.data if line[0] == key]
+        return [MaskCSVRow(
+            line[0], # img
+            int(line[1]), # img_height
+            int(line[2]), # img_width
+            int(line[3]), # face_xtl
+            int(line[4]), # face_ytl
+            int(line[5]), # face_xbr
+            int(line[6]), # face_ybr
+            int(line[7]), # mask_xtl
+            int(line[8]), # mask_ytl
+            int(line[9]), # mask_xtm
+            int(line[10]), # mask_ytm
+            int(line[11]), # mask_xtr
+            int(line[12]), # mask_ytr
+            int(line[13]), # mask_xbr
+            int(line[14]), # mask_ybr
+            int(line[15]), # mask_xbm
+            int(line[16]), # mask_ybm
+            int(line[17]), # mask_xbl
+            int(line[18]) # mask_ybl
+                ) for line in self.data if line[0] == key]
 
 
 class Resizer:
@@ -43,15 +87,12 @@ class Resizer:
         self.mode = "scale" # "keep og"
         self.background = (0, 0, 0) # BGR not RGB
         # Technically Configurable but less commonly changed
-        self.data_dir = "./data"
-        self.labels_dir = self.data_dir + "/labels"
-        self.images_dir = self.data_dir + "/images"
-        self.normal_dir = self.images_dir + "/normal_faces"  # ground truth
-        self.masks_dir = self.images_dir + "/masks" # pictures of masks that are overlayed
-        self.masked_dir = self.images_dir + "/masked_faces" # edited photos with masks
-        self.resized_truth_dir = self.images_dir + "/rs_truth" # resized ground truth
-        self.resized_masked_dir = self.images_dir + "/rs_masked" # resized pics with edited masks
-        self.resized_masks_dir = self.images_dir + "/rs_masks" # resized pics with the mask (what gets removed)
+        self.mask_labels = "../MaskTheFaceNew/mask.csv"
+        self.normal_dir = "../MaskTheFaceNew/normal_faces"  # ground truth
+        self.masked_dir = "../MaskTheFaceNew/normal_faces_masked" # edited photos with masks
+        self.resized_truth_dir = "./data/images/rs_truth" # resized ground truth
+        self.resized_masked_dir = "./data/images/rs_masked" # resized pics with edited masks
+        self.resized_masks_dir = "./data/images/rs_masks" # resized pics with the mask (what gets removed)
 
         if not os.path.exists(self.resized_truth_dir):
             os.makedirs(self.resized_truth_dir)
@@ -61,16 +102,19 @@ class Resizer:
             os.makedirs(self.resized_masks_dir)
 
     def do_all(self):
-        labels_masked_faces = CSVFile(self.labels_dir + '/masked_faces.csv')
+        labels_masked_faces = CSVFile(self.mask_labels)
         labels_masked_faces.open()
         unique_keys = labels_masked_faces.get_unique_keys()
         for u_key in unique_keys:
+            # this image is first when alphabetically sorted
+            #if u_key != "00a0d634ad200ced.jpg":
+            #    continue
             print(u_key)
             normal_face_image_path = self.normal_dir + "/" + u_key
-            masked_face_image_path = self.masked_dir + "/" + u_key
+            masked_face_image_path = self.masked_dir + "/" + u_key.split('.')[0] + '_surgical.jpg'
             resized_truth_path = self.resized_truth_dir + "/" + u_key
             resized_mask_channel_path = self.resized_masks_dir + "/" + u_key
-            # IMREAD_UNCHANGED: preserve alpha channel
+            resized_masked_path = self.resized_masked_dir + "/" + u_key.split('.')[0] + '_surgical.jpg'
             normal_face_image = cv2.imread(normal_face_image_path, cv2.IMREAD_COLOR)
             masked_face_image = cv2.imread(masked_face_image_path, cv2.IMREAD_COLOR)
             if self.mode == "scale":
@@ -87,23 +131,56 @@ class Resizer:
                 scaling_factor = self.height / og_height if image_orientation == "vertical" else self.width / og_width
                 scaled_height = int(og_height * scaling_factor)
                 scaled_width = int(og_width * scaling_factor)
+                # resize image
                 im_resized_truth = cv2.resize(normal_face_image, (scaled_height, scaled_width), interpolation=cv2.INTER_AREA)
+                # create the background
                 background = np.zeros((self.width, self.height, 3), np.uint8)
                 background[:] = self.background
+                # paste image onto background (black bars for aspect ratio)
                 y_offset = 0
                 x_offset= 0
                 background[y_offset:y_offset+im_resized_truth.shape[0], x_offset:x_offset+im_resized_truth.shape[1]] = im_resized_truth
                 # output_truth = cv2.addWeighted(background, 1.0, im_resized_truth, 1.0, 0)
                 cv2.imwrite(resized_truth_path, background)
 
-                # create mask image
+                # create mask channel image
                 mask_channel = np.zeros((og_width, og_height, 3), np.uint8)
                 mask_channel[:] = (0, 0, 0)
-                for mask in labels_masked_faces.get_all_where_key_equals(u_key):
-                    (xmin, ymin, xmax, ymax) = (mask[2], mask[3], mask[4], mask[5])
-                    # TODO: might need to do height - y because of opencv2's top-down logic
-                    cv2.rectangle(mask_channel, (xmin, ymin), (xmax, ymax), (255, 255, 255), -1)
-                cv2.imwrite(resized_mask_channel_path, mask_channel)
+                mask_bounding_boxes = labels_masked_faces.get_all_where_key_equals(u_key)
+                for mask in mask_bounding_boxes:
+                    # opencv2's top-down logic
+                    cv2.rectangle(mask_channel,
+                                  (
+                                      min(mask.mask_xtl, mask.mask_xbl, mask.mask_xtm, mask.mask_xbm),
+                                      min(mask.mask_ytl, mask.mask_ytr, mask.mask_ytm)
+                                  ),
+                                  (
+                                      max(mask.mask_xbr, mask.mask_xtr),
+                                      max(mask.mask_ybl, mask.mask_ybm, mask.mask_ybr)
+                                  ),
+                                  (255, 255, 255), -1)
+
+                # resize image
+                mask_channel = cv2.resize(mask_channel, (scaled_height, scaled_width), interpolation=cv2.INTER_AREA)
+                # create the background
+                background = np.zeros((self.width, self.height, 3), np.uint8)
+                background[:] = self.background
+                # paste image onto background (black bars for aspect ratio)
+                y_offset = 0
+                x_offset= 0
+                background[y_offset:y_offset+mask_channel.shape[0], x_offset:x_offset+mask_channel.shape[1]] = mask_channel
+                cv2.imwrite(resized_mask_channel_path, background)
+
+                # resize image
+                masked_face_image = cv2.resize(masked_face_image, (scaled_height, scaled_width), interpolation=cv2.INTER_AREA)
+                # create the background
+                background = np.zeros((self.width, self.height, 3), np.uint8)
+                background[:] = self.background
+                # paste image onto background (black bars for aspect ratio)
+                y_offset = 0
+                x_offset= 0
+                background[y_offset:y_offset+mask_channel.shape[0], x_offset:x_offset+mask_channel.shape[1]] = masked_face_image
+                cv2.imwrite(resized_masked_path, background)
 
 if __name__ == "__main__":
     resizer = Resizer(800, 800)
