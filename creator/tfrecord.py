03c4de64f9b9d4818de4c21f4d7488b1f14c85fd
JCharante <git@jcharante.com>
2021-11-23 20:18:59 -0500

    adapt tfrecord (discriminator) script for new dataset

--- creator/tfrecord.py
+++ creator/tfrecord.py
@@ -15,13 +15,13 @@ def create_tf_example(filename, rows):
     filename = filename.encode('utf8')
     image_format = b'jpeg'
 
-    xmins = [i["xmin"] for i in rows]  # List of normalized left x coordinates in bounding box (1 per box)
-    xmaxs = [i["xmax"] for i in rows]  # List of normalized right x coordinates in bounding box
+    xmins = [min(i['mask_xtl'], i['mask_xbl'], i['mask_xtm'], i['mask_xbm']) for i in rows]  # List of normalized left x coordinates in bounding box (1 per box)
+    xmaxs = [max(i['mask_xbr'], i['mask_xtr']) for i in rows]  # List of normalized right x coordinates in bounding box
     # (1 per box)
-    ymins = [i["ymin"] for i in rows]  # List of normalized top y coordinates in bounding box (1 per box)
-    ymaxs = [i["ymax"] for i in rows]  # List of normalized bottom y coordinates in bounding box
+    ymins = [min(i['mask_ytl'], i['mask_ytr'], i['mask_ytm']) for i in rows]  # List of normalized top y coordinates in bounding box (1 per box)
+    ymaxs = [max(i['mask_ybl'], i['mask_ybm'], i['mask_ybr']) for i in rows]  # List of normalized bottom y coordinates in bounding box
     # (1 per box)
-    classes_text = [i["label"].encode("utf8") for i in rows]  # List of string class name of bounding box (1 per box)
+    classes_text = ['mask'.encode("utf8") for i in rows]  # List of string class name of bounding box (1 per box)
     classes = [1 for _ in rows]  # List of integer class id of bounding box (1 per box)
 
     tf_example = tf.train.Example(features=tf.train.Features(feature={
@@ -44,16 +44,16 @@ def create_tf_example(filename, rows):
 def main():
     images = {}
     writer = tf.compat.v1.python_io.TFRecordWriter("data/tf/masks.tfrecord")
-    df = pd.read_csv("data/labels/masked_faces.csv")
+    df = pd.read_csv("../MaskTheFaceNew/mask.csv")
     for index, row in df.iterrows():
-        key = row["key"]
+        key = row["img"]
         try:
             images[key].append(row)
         except KeyError:
             images.update({key: [row]})
     examples = []
     for image in images.keys():
-        example = create_tf_example("data/images/masked_faces/" + key, images[image])
+        example = create_tf_example("../MaskTheFaceNew/normal_faces_masked/" + key.split('.')[0] + "_surgical.jpg", images[image])
         examples.append(example)
 
     for example in examples:
